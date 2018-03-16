7080344e2625ef2f577b939f0c2215c570255086
Stephen Fowler <stephen.fowler@changepoint.com>
2018-03-16 12:26:30 -0600

    Adding layouts but not able to overlay

--- src/App.js
+++ src/App.js
@@ -12,11 +12,17 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to ParallaxBot</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='container' >
+          <Background className='bk'>
+            <Mountains className='mid'>
+              {/* <div className='fore' style={{ background: RollingHills }}>
+                <div className='figure' style={{ background: Robot }}></div>
+              </div> */}
+            </Mountains>
+          </Background>
+        </div>
       </div>
     );
   }
